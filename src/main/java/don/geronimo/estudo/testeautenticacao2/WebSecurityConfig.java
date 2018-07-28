d2cf3836f8fec59add1d2003b6fc740fa729e3ab
geronimo-lisboa <erikaeluciano2016@gmail.com>
2018-07-28 11:37:44 -0300

    Com isso aqui eu consigo fazer com que a /login só seja acessível via POST.

--- src/main/java/don/geronimo/estudo/testeautenticacao2/WebSecurityConfig.java
+++ src/main/java/don/geronimo/estudo/testeautenticacao2/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package don.geronimo.estudo.testeautenticacao2;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -11,6 +12,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity httpSecurity)throws Exception{
         System.out.println("WebSecurityConfig.configure(httpSecurity)");
+        httpSecurity.csrf().disable().authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/login").permitAll();
+
     }
 
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
